fd5454440b7ad3f9640c020910e9349d4f2c5e7c
ErnestoVera <veraem.sw@gmail.com>
2021-11-25 01:31:20 -0800

    Build para experimentos de validación del simulador.

--- app/src/main/java/mx/uabc/ahrs/SensorActivity.java
+++ app/src/main/java/mx/uabc/ahrs/SensorActivity.java
@@ -229,7 +229,7 @@ public class SensorActivity extends AppCompatActivity {
 
             }
 
-        } else if (parts.length == 3 && lecturaActual == ORIENTATION_INDEX) {
+        } else if (parts.length == 3) {// && lecturaActual == ORIENTATION_INDEX) {
             double pitch, roll, yaw;
 
             try {
@@ -248,11 +248,11 @@ public class SensorActivity extends AppCompatActivity {
 
             String msg = pitch + "," + roll + "\r\n";
             sendMessage(msg);
-            lecturaActual = (lecturaActual + 1)%2;
+            //lecturaActual = (lecturaActual + 1)%2;
             long timestamp = System.currentTimeMillis();
-            EventBus.getDefault().post(new SensorReadingEvent(pitch, roll, lastY, lastZ, magGyro, timestamp));
+            EventBus.getDefault().post(new SensorReadingEvent(pitch, roll, lastY, lastZ, 0, timestamp));
         }
-        else if(parts.length == 3 && lecturaActual == GYROSCOPE_INDEX) {
+        /*else if(parts.length == 3 && lecturaActual == GYROSCOPE_INDEX) {
             double pitch, roll, yaw;
             try {
 
@@ -270,7 +270,7 @@ public class SensorActivity extends AppCompatActivity {
             lecturaActual = (lecturaActual + 1)%2;
             magGyro = Math.sqrt(pitch*pitch + yaw*yaw + roll*roll);
 
-        }
+        }*/
     }
 
     private void sendMessage(String message) {
@@ -342,7 +342,8 @@ public class SensorActivity extends AppCompatActivity {
     }
 
     private void startSensorStreaming() {
-        sendSensorMessage(":80,38,7,255,255,255,255,255,255\n");
+        //sendSensorMessage(":80,38,7,255,255,255,255,255,255\n");
+        sendSensorMessage(":80,7,255,255,255,255,255,255,255\n");
         sendSensorMessage(":82,25000,-1,0\n");
         sendSensorMessage(":85\n");
     }
